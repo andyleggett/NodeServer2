8fb0acc6b95f6555d77956420ae61755fba474a7
Andrew Leggett <me@andyleggett.co.uk>
2014-03-25 08:38:23 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var net = require("net"),
+port = Number(process.env.PORT || 6100);
+
+
+var relay = net.createServer(function (socket) {
+
+	console.log("render socket connected");
+
+	socket.on("data", function(data){
+
+		console.log("rendering: " + data);
+		socket.write("render successful");
+	});
+
+	socket.on("close", function(data){
+
+		console.log("closed");
+	});
+
+}).listen(port);
